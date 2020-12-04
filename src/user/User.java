8a06bb0feb486f40a0488e857e24570c5a2b8353
andreikoblicica <andrei.kobli@gmail.com>
2020-12-04 18:54:20 +0200

    some classes

--- src/user/User.java
+++ src/user/User.java
@@ -1,23 +1,97 @@
 package user;
 
+
+import java.util.ArrayList;
+
 public abstract class User {
 
 	private int id;
-	private String name;
+	private String username;
 	private String password;
 	private String country;
 	private String email;
-	int attempedProblems;
-	
-	double rating;
+	private ArrayList<Integer> attemptedProblems;
+	private Statistics statistics;
 
-	public User() {
-		// TODO Auto-generated constructor stub
+
+	public User(int id, String username, String password, String country, String email, ArrayList<Integer> attemptedProblems, Statistics statistics) {
+		this.id=id;
+		this.password=password;
+		this.username=username;
+		this.country=country;
+		this.email=email;
+		this.attemptedProblems=attemptedProblems;
+		this.statistics=statistics;
 	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public ArrayList<Integer> getAttempedProblems() {
+		return attemptedProblems;
+	}
+
+	public void setAttempedProblems(ArrayList<Integer> attempedProblems) {
+		this.attemptedProblems = attempedProblems;
+	}
+
+	public ArrayList<Integer> getAttemptedProblems() {
+		return attemptedProblems;
+	}
+
+	public void setAttemptedProblems(ArrayList<Integer> attemptedProblems) {
+		this.attemptedProblems = attemptedProblems;
+	}
+
+	public Statistics getStatistics() {
+		return statistics;
+	}
+
+	public void setStatistics(Statistics statistics) {
+		this.statistics = statistics;
+	}
+
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 	}
 
 }
