5c1d942d660bf6e8b655db9dd137a728d32eb37e
Miruna Hulpe <miruhulpe@yahoo.com>
2020-12-05 23:44:44 +0200

    ProposeProblem fxml file

--- src/ui/ProposeProblem.java
+++ src/ui/ProposeProblem.java
@@ -3,12 +3,9 @@ package ui;
 public class ProposeProblem {
 
 	public ProposeProblem() {
-		// TODO Auto-generated constructor stub
-	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
+
+    
 
 }
