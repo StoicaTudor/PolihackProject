6967c561f863ac28175588a15038bc5dab83d9a8
StoicaTudor <56082968+StoicaTudor@users.noreply.github.com>
2020-12-06 00:00:59 +0200

    Tudor Commit 00:00

---
+++ src/database/Utility.java
@@ -0,0 +1,14 @@
+package database;
+
+public class Utility {
+
+	public Utility() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/user/Student.java
+++ src/user/Student.java
@@ -1,26 +1,42 @@
 package user;
 
+import problem.Difficulty;
+import problem.Problem;
 import problem.Subject;
 
 import java.util.ArrayList;
 import java.util.Set;
 
-public class Student extends User{
+public class Student extends User {
 
+	int currentGrade;
 
-	public Student(int id, String username, String password, String country, String email, ArrayList<Integer> attemptedProblems, Statistics statistics, Set<Subject> preferredSubjects) {
-		super(id,username,password,country,email,attemptedProblems,statistics,preferredSubjects);
+	public Student(int id, String username, String password, String country, String email,
+			ArrayList<Integer> attemptedProblems, Statistics statistics, Set<Subject> preferredSubjects, int grade) {
+
+		super(id, username, password, country, email, attemptedProblems, statistics, preferredSubjects);
+		this.currentGrade = currentGrade;
+	}
+
+	public void solveProblem() {
 
 	}
-	public void solveProblem(){
+
+	public void submitSolution() {
 
 	}
-	public void submitSolution(){
 
-	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+
+	// initially, this method will be called by the student's preferred subjects and
+	// student's grade
+	// additionally, we MUST check for the problems which are NOT attempted
+	public Set<Problem> filterProblems(Set<Subject> filteredSubjects, int filterGrade, Difficulty filteredDifficulty) {
+		// TO DO MIRUNA
+		return null;
 	}
 
 }
