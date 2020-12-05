af26a05762d83b9a8bda33e65f17edde89f38634
StoicaTudor <56082968+StoicaTudor@users.noreply.github.com>
2020-12-05 11:00:04 +0200

    DatabaseWork

--- src/database/DataFromDatabase.java
+++ src/database/DataFromDatabase.java
@@ -3,12 +3,17 @@ package database;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import user.User;
 
 public class DataFromDatabase {
 
 	private Connection connection;
 	private Statement statement;
 	private ResultSet resultSet;
+	public List<User> user=new ArrayList<>();
 
 	public DataFromDatabase(Connection connection, Statement statement, ResultSet resultSet) {
 
