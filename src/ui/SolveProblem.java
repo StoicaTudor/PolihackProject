de2061ff3c3ad566ab42adf6d101e7b9c16c002e
StoicaTudor <56082968+StoicaTudor@users.noreply.github.com>
2020-12-06 13:39:49 +0200

    tudor update

--- src/ui/SolveProblem.java
+++ src/ui/SolveProblem.java
@@ -14,9 +14,6 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-
-
-
 public class SolveProblem {
 
 	@FXML
@@ -33,39 +30,39 @@ public class SolveProblem {
 	int problemID;
 	Scene scene;
 
-	public void setScene(ActionEvent event, DataFromDatabase data,int problemID) {
-		this.data=data;
-		this.problemID=problemID;
-		Parent menu=null;
+	public void setScene(ActionEvent event, DataFromDatabase data, int problemID) {
+		this.data = data;
+		this.problemID = problemID;
+		Parent menu = null;
 		try {
-			menu = FXMLLoader.load(getClass().getResource("solveProblem.fxml")); //pui fxml de la scena pe care vrei sa o incarci
+			menu = FXMLLoader.load(getClass().getResource("solveProblem.fxml")); // pui fxml de la scena pe care vrei sa
+																					// o incarci
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		 scene= new Scene(menu);
-		IDLabel=(Label)scene.lookup("#IDLabel");
-		StringBuilder id=new StringBuilder(problemID);
+		scene = new Scene(menu);
+		IDLabel = (Label) scene.lookup("#IDLabel");
+		StringBuilder id = new StringBuilder(problemID);
 		id.toString();
-		IDLabel.setText("ID #"+ id);
-		Stage window=(Stage)((Node)event.getSource()).getScene().getWindow();
+		IDLabel.setText("ID #" + id);
+		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
 		window.setScene(scene);
 		window.show();
 	}
 
-	public void clickSubmit(ActionEvent event){
-		//submit button clicked
-		//insert problem into DB
-		task=(TextArea)scene.lookup("#task");
-		//data.submitSolution(problemID,task.getText());
-		StudentMenu studentMenu=new StudentMenu();
-		studentMenu.setScene(event,data);
-	}
-	public void clickBack(ActionEvent event){
-
-		StudentMenu studentMenu=new StudentMenu();
-		studentMenu.setScene(event,data);
+	public void clickSubmit(ActionEvent event) {
+		// submit button clicked
+		// insert problem into DB
+		task = (TextArea) scene.lookup("#task");
+		// data.submitSolution(problemID,task.getText());
+		StudentMenu studentMenu = new StudentMenu();
+		studentMenu.setScene(event, data);
 	}
 
+	public void clickBack(ActionEvent event) {
 
+		StudentMenu studentMenu = new StudentMenu();
+		studentMenu.setScene(event, data);
+	}
 
 }
